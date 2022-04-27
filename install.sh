d0a0915a9aed1a5910182babbedb85f3ba6abfbc
Long_yi <68991513+longyi8@users.noreply.github.com>
2022-04-28 01:05:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -181,7 +181,6 @@ install_XrayR() {
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
-    systemctl unmask XrayR
     echo -e ""
     echo "XrayR 管理脚本使用方法 (兼容使用xrayr执行，大小写不敏感): "
     echo "------------------------------------------"
